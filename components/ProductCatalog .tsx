2d325b89b435e6f4c21e60201f739ffc244502a2
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-06-21 18:29:14 +0300

    implement product detail page and related products functionality

--- components/ProductCatalog .tsx
+++ components/ProductCatalog .tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-import { IProduct } from "@/types/product";
 import ProductCard from "./ProductCard";
+import { IProductEntity } from "oneentry/dist/products/productsInterfaces";
+import { IProduct } from "@/types/product";
 
 
-const ProductCatalog = ({ title, products }: { title: string; products: IProduct[] }) => {
+const ProductCatalog = ({ title, products }: { title: string; products: IProductEntity[] | IProduct[] }) => {
   return (
     <section className="mb-12">
       <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent">
@@ -12,7 +13,7 @@ const ProductCatalog = ({ title, products }: { title: string; products: IProduct
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-        {products?.map((product: IProduct) => (
+        {products?.map((product: IProductEntity | IProduct) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
